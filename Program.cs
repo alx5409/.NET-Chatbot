526be0773a1a821d6bc9531eac6f7841aacd98de
Alex <alx5409@gmail.com>
2025-11-10 16:49:50 +0100

    Add minimal functions to start the chatbot

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using System;
+using Chatbot;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Simple Chatbot!");
+        var cbot = new Chatbot.Chatbot();
         while (true)
         {
             Console.Write("You: ");
@@ -14,6 +16,8 @@ class Program
                 break;
             }
             Console.WriteLine("Chatbot: ");
+            Console.WriteLine(cbot.GetResponse(userInput));
+            
         }
         Console.WriteLine("Goodbye!");
         Console.ReadLine();
